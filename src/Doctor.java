e055a4b07806c1a8ee82b0d93537131be6780775
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-05-14 15:42:11 -0700

    Create UI Menu

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,17 @@
 public class Doctor {
-  int id;
+  static int id = 0;
   String name;
   String specialization;
 
+  Doctor() {
+    id++;
+  }
+
   public void showName() {
     System.out.println(name);
+  }
+
+  public void showId() {
+    System.out.println(id);
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static UI.UIMenu.*;
+
 public class Main {
   public static void main(String[] args) {
-    Doctor myDoctor = new Doctor();
-    myDoctor.name = "Alejandro Rodriguez";
-    myDoctor.showName();
+    showMenu();
   }
 }
