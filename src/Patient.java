f008b0412804f5d7aa5029b1de83f442113bb92f
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-05-22 10:34:38 -0700

    Get and Set Patient Class

--- src/Patient.java
+++ src/Patient.java
@@ -14,4 +14,76 @@ public class Patient {
     this.name = name;
     this.email = email;
   }
+
+  public void setWeight(double weight) {
+    this.weight = weight;
+  }
+
+  public String  getWeight() {
+    return Double.toString(weight);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(String birthday) {
+    this.birthday = birthday;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public void setHeight(double height) {
+    this.height = height;
+  }
+
+  public String getBlood() {
+    return blood;
+  }
+
+  public void setBlood(String blood) {
+    this.blood = blood;
+  }
 }
