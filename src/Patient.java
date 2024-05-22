1c37f5c59401dce6d30459666a8ee256eeb2ac47
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-05-22 14:59:52 -0700

    Refactor Patient Class

--- src/Patient.java
+++ src/Patient.java
@@ -15,75 +15,72 @@ public class Patient {
     this.email = email;
   }
 
-  public void setWeight(double weight) {
-    this.weight = weight;
-  }
-
-  public String  getWeight() {
-    return Double.toString(weight);
-  }
-
-  public int getId() {
-    return id;
-  }
-
   public void setId(int id) {
     this.id = id;
   }
-
-  public String getName() {
-    return name;
+  public int getId() {
+    return id;
   }
 
   public void setName(String name) {
     this.name = name;
   }
-
-  public String getEmail() {
-    return email;
+  public String getName() {
+    return name;
   }
 
   public void setEmail(String email) {
     this.email = email;
   }
-
-  public String getAddress() {
-    return address;
+  public String getEmail() {
+    return email;
   }
 
   public void setAddress(String address) {
     this.address = address;
   }
-
-  public String getPhoneNumber() {
-    return phoneNumber;
+  public String getAddress() {
+    return address;
   }
 
   public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
+    if (phoneNumber.length() == 10) {
+      this.phoneNumber = phoneNumber;
+    }else {
+      System.out.println("Invalid phone number");
+    }
   }
-
-  public String getBirthday() {
-    return birthday;
+  public String getPhoneNumber() {
+    return phoneNumber;
   }
 
   public void setBirthday(String birthday) {
     this.birthday = birthday;
   }
+  public String getBirthday() {
+    return birthday;
+  }
 
-  public double getHeight() {
-    return height;
+  public void setWeight(double weight) {
+    this.weight = weight;
+  }
+  public String  getWeight() {
+    return Double.toString(weight);
   }
 
   public void setHeight(double height) {
     this.height = height;
   }
-
-  public String getBlood() {
-    return blood;
+  public double getHeight() {
+    return height;
   }
 
   public void setBlood(String blood) {
     this.blood = blood;
   }
+  public String getBlood() {
+    return blood;
+  }
+
+
 }
